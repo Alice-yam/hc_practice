5d596fa2c1e247b2b42c1c6c1300cf537f9af795
Alice-yam <yama2alc@gmail.com>
2026-05-19 19:30:36 +0900

    Fix add option check, padStart method

--- js_calender/calendar.js
+++ js_calender/calendar.js
@@ -6,10 +6,15 @@ let month;
 if (process.argv.length === 2) {
   month = now.getMonth() + 1; // 0が1月のため+1する
 } else if (process.argv.length === 4) {
+  // オプションが適切に入力されているかチェック
+  if (process.argv[2] !== "-m") {
+    console.error("入力エラー");
+    process.exit(1);
+  }
   month = Number(process.argv[3]);
 }
 // 1 ~ 12以外を入力するとエラー
-if (month < 1 || month > 12) {
+if (month < 1 || month > 12 || month === undefined) {
   console.error("入力エラー");
   process.exit(1);
 }
@@ -34,17 +39,23 @@ for (let i = 0; i < weekDays; i++) {
 }
 
 // 1桁の日付は前後にスペース、2桁の日付は後ろにのみスペース
+
 for (let date = 1; date <= lastDay.getDate(); date++) {
-  if (date < 10) {
-    // date = ` ${date}`;
-    calendarLine += " " + date + " ";
-  } else {
-    calendarLine += date + " "; //から、足していって横に並ばせる
-  }
+  // dateの文字列を2桁にして足りない分を" "で埋める
+  let daySpace = String(date).padStart(2, " ");
+  calendarLine += daySpace + " ";
+  // if (date < 10) {
+  //   // date = ` ${date}`;
+  //   calendarLine += " " + date + " "; //から、足していって横に並ばせる
+  // } else {
+  //   calendarLine += date + " "; //こっちも同様に足す
+  // }
+
   // 1週間ごとに改行させる
   if ((weekDays + date) % 7 === 0) {
     console.log(calendarLine);
     calendarLine = "";
   }
 }
+
 console.log(calendarLine);
